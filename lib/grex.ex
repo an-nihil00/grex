1a49121078ff1f2bb12c6d612fccc773d59b724e
an-nihil00 <32604310+an-nihil00@users.noreply.github.com>
2020-12-30 18:20:43 -0500

    initial mix project creation

---
+++ lib/grex.ex
@@ -0,0 +1,18 @@
+defmodule Grex do
+  @moduledoc """
+  Documentation for `Grex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Grex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
--- shell.nix
+++ shell.nix
@@ -2,6 +2,10 @@
 
 with pkgs;
 
+let
+  inherit (lib) optional optionals;
+in
+
 mkShell {
   buildInputs = [ elixir git postgresql ]
                 ++ optional stdenv.isLinux libnotify # For ExUnit Notifier on Linux.
